2672785ec7a2495620afdb3a294f27b29e718c18
Joey Tam <joeytam18@gmail.com>
2025-10-31 14:46:46 -0400

    Changed populate_data.py to sync genre, album and artist

--- populate_data.py
+++ populate_data.py
@@ -224,104 +224,230 @@ def populate_follow_users(con, n=5000):
     cur.executemany("INSERT INTO followuser(follow_username, followed_username) VALUES (%s, %s)", data)
     con.commit()
 
-def populate_make_song(con, n=5000):
+def populate_make_song(con):
     cur = con.cursor()
     cur.execute("SELECT artist_id FROM artist")
     artists = [row[0] for row in cur.fetchall()]
     cur.execute("SELECT song_id FROM song")
     songs = [row[0] for row in cur.fetchall()]
+    cur.execute("SELECT artist_id, song_id FROM makesong")
+    artist_song = set(cur.fetchall())
     
     data = []
-    for song_id in songs:
-        selected_artists = random.sample(artists, k=random.randint(1,3))
-        for artist_id in selected_artists:
-            if len(data) >= n:
-                break
+    song_to_artists = {song_id: set() for song_id in songs}
+    artist_to_songs = {artist_id: set() for artist_id in artists}
+
+    for artist_id, song_id in artist_song:
+        song_to_artists[song_id].add(artist_id)
+        artist_to_songs[artist_id].add(song_id)
+
+    for artist_id in artists:
+        if not artist_to_songs[artist_id]:
+            song_id = random.choice(songs)
             data.append((artist_id, song_id))
-        if len(data) >= n:
-            break
+            song_to_artists[song_id].add(artist_id)
+            artist_to_songs[artist_id].add(song_id)
     
-    cur.executemany("INSERT INTO makesong(artist_id, song_id) VALUES (%s, %s)", data)
+    for song_id in songs:
+        has_song = song_to_artists[song_id] #artists that have a song
+        need = random.randint(1, 2)
+        if len(has_song) < need:
+            avail_artists = [artist for artist in artists if artist not in has_song]
+            count = min(need - len(has_song), len(avail_artists))
+            new_artists = random.sample(avail_artists, k=count)
+            for artist_id in new_artists:
+                data.append((artist_id, song_id))
+                song_to_artists[song_id].add(artist_id)
+                artist_to_songs[artist_id].add(song_id)
+    
+    cur.executemany("INSERT INTO makesong(artist_id, song_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", data)
     con.commit()
 
-def populate_make_album(con, n=5000):
+def populate_make_album(con):
     cur = con.cursor()
     cur.execute("SELECT artist_id FROM artist")
     artists = [row[0] for row in cur.fetchall()]
     cur.execute("SELECT album_id FROM album")
     albums = [row[0] for row in cur.fetchall()]
     
+    cur.execute("SELECT artist_id, album_id FROM makealbum")
+    artist_album = set(cur.fetchall())
+    
+    artist_to_albums = {artist_id: set() for artist_id in artists}
+    album_to_artists = {album_id: set() for album_id in albums}
+    for artist_id, album_id in artist_album:
+        artist_to_albums[artist_id].add(album_id)
+        album_to_artists[album_id].add(artist_id)
     data = []
-    for album_id in albums:
-        selected_artists = random.sample(artists, k=random.randint(1,2))
-        for artist_id in selected_artists:
-            if len(data) >= n:
-                break
+
+    for artist_id in artists:
+        if not artist_to_albums[artist_id]:
+            album_id = random.choice(albums)
             data.append((artist_id, album_id))
-        if len(data) >= n:
-            break
+            artist_to_albums[artist_id].add(album_id)
+            album_to_artists[album_id].add(artist_id)
     
-    cur.executemany("INSERT INTO makealbum(artist_id, album_id) VALUES (%s, %s)", data)
+    for album_id in albums:
+        if not album_to_artists[album_id]:
+            artist_id = random.choice(artists)
+            data.append((artist_id, album_id))
+            artist_to_albums[artist_id].add(album_id)
+            album_to_artists[album_id].add(artist_id)
+    
+    for album_id in albums:
+        avail_artists = [artist for artist in artists if artist not in album_to_artists[album_id]]
+        if avail_artists:
+            new_artists = random.sample(avail_artists, k=min(len(avail_artists), random.randint(1, 2)))
+            for artist_id in new_artists:
+                if(artist_id, album_id) not in artist_album and (artist_id, album_id) not in data:
+                    data.append((artist_id, album_id))
+                    artist_to_albums[artist_id].add(album_id)
+                    album_to_artists[album_id].add(artist_id)
+    
+    cur.executemany("INSERT INTO makealbum(artist_id, album_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", data)
     con.commit()
 
-def populate_is_part_of_album(con, n=5000):
+def populate_is_part_of_album(con):
     cur = con.cursor()
     cur.execute("SELECT song_id FROM song")
     songs = [row[0] for row in cur.fetchall()]
     cur.execute("SELECT album_id FROM album")
     albums = [row[0] for row in cur.fetchall()]
     
+    song_to_albums = {song_id: set() for song_id in songs}
+    album_to_songs = {album_id: set() for album_id in albums}
     data = []
-    for album_id in albums:
-        album_songs = random.sample(songs, k=random.randint(3,8))
-        for track_num, song_id in enumerate(album_songs, start=1):
-            if len(data) >= n:
-                break
-            data.append((song_id, album_id, track_num))
-        if len(data) >= n:
-            break
-    
-    cur.executemany("INSERT INTO ispartofalbum(song_id, album_id, track_num) VALUES (%s, %s, %s)", data)
-    con.commit()
 
-def populate_song_has_genre(con, n=5000):
-    cur = con.cursor()
-    cur.execute("SELECT song_id FROM song")
-    songs = [row[0] for row in cur.fetchall()]
-    cur.execute("SELECT genre_id FROM genre")
-    genres = [row[0] for row in cur.fetchall()]
-    
-    data = []
     for song_id in songs:
-        selected_genres = random.sample(genres, k=random.randint(1,3))
-        for genre_id in selected_genres:
-            if len(data) >= n:
-                break
-            data.append((song_id, genre_id))
-        if len(data) >= n:
-            break
+        need = random.randint(1, 2)
+        avail_albums = [album for album in albums if album not in song_to_albums[song_id]]
+        count = min(need, len(avail_albums))
+        selected_albums = random.sample(avail_albums, count)
+        for album_id in selected_albums:
+            track_num = len(album_to_songs[album_id]) + 1
+            data.append((song_id, album_id, track_num))
+            song_to_albums[song_id].add(album_id)
+            album_to_songs[album_id].add(song_id)
     
-    cur.executemany("INSERT INTO songhasgenre(song_id, genre_id) VALUES (%s, %s)", data)
+    for album_id in albums:
+        if not album_to_songs[album_id]:
+            song_id = random.choice(songs)
+            data.append((song_id, album_id, 1))
+            song_to_albums[song_id].add(album_id)
+            album_to_songs[album_id].add(song_id)
+    
+    cur.executemany("INSERT INTO ispartofalbum(song_id, album_id, track_num) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING", data)
     con.commit()
 
-def populate_album_has_genre(con, n=5000):
+def populate_song_has_genre(con):
+    cur = con.cursor()
+    cur.execute("SELECT song_id FROM song")
+    songs = [row[0] for row in cur.fetchall()]
+    cur.execute("SELECT genre_id FROM genre")
+    genres = [row[0] for row in cur.fetchall()]
+
+    song_to_genres = {song_id: set() for song_id in songs}
+    genre_to_songs = {genre_id: set() for genre_id in genres}
+    data = []
+
+    for song_id in songs:
+        need = random.randint(1, 3)
+        avail_genres = [genre for genre in genres if genre not in song_to_genres[song_id]]
+        count = min(need, len(avail_genres))
+        selected_genres = random.sample(avail_genres, count)
+        for genre_id in selected_genres:
+            data.append((song_id, genre_id))
+            song_to_genres[song_id].add(genre_id)
+            genre_to_songs[genre_id].add(song_id)
+    
+    cur.executemany("INSERT INTO songhasgenre(song_id, genre_id) VALUES (%s, %s) ON CONFLICT DO NOTHING", data)
+    con.commit()
+
+def populate_album_has_genre(con):
     cur = con.cursor()
     cur.execute("SELECT album_id FROM album")
     albums = [row[0] for row in cur.fetchall()]
     cur.execute("SELECT genre_id FROM genre")
     genres = [row[0] for row in cur.fetchall()]
     
+    album_to_genres = {album_id: set() for album_id in albums}
+    genre_to_albums = {genre_id: set() for genre_id in genres}
     data = []
+
     for album_id in albums:
-        selected_genres = random.sample(genres, k=random.randint(1,2))
+        need = random.randint(1, 2)
+        avail_genres = [genre for genre in genres if genre not in album_to_genres[album_id]]
+        count = min(need, len(avail_genres))
+        selected_genres = random.sample(avail_genres, count)
         for genre_id in selected_genres:
-            if len(data) >= n:
-                break
             data.append((album_id, genre_id))
-        if len(data) >= n:
-            break
+            album_to_genres[album_id].add(genre_id)
+            genre_to_albums[genre_id].add(album_id)
     
-    cur.executemany("""INSERT INTO albumhasgenre(album_id, genre_id) VALUES (%s, %s)  ON CONFLICT DO NOTHING;""", data)
+    cur.executemany("INSERT INTO albumhasgenre(album_id, genre_id) VALUES (%s, %s)  ON CONFLICT DO NOTHING" , data)
+    con.commit()
+
+def sync_music(con):
+    cur = con.cursor()
+
+    cur.execute("SELECT song_id, album_id FROM ispartofalbum")
+    album_songs = {}
+    for song_id, album_id in cur.fetchall():
+        album_songs.setdefault(album_id, set()).add(song_id)
+
+    cur.execute("SELECT artist_id, song_id FROM makesong")
+    song_artists = {}
+    for artist_id, song_id in cur.fetchall():
+        song_artists.setdefault(song_id, set()).add(artist_id)
+
+    cur.execute("SELECT song_id, genre_id FROM songhasgenre")
+    song_genres = {}
+    for song_id, genre_id in cur.fetchall():
+        song_genres.setdefault(song_id, set()).add(genre_id)
+
+    cur.execute("SELECT artist_id, album_id FROM makealbum")
+    album_artists = {}
+    for artist_id, album_id in cur.fetchall():
+        album_artists.setdefault(album_id, set()).add(artist_id)
+
+    cur.execute("SELECT album_id, genre_id FROM albumhasgenre")
+    album_genres = {}
+    for album_id, genre_id in cur.fetchall():
+        album_genres.setdefault(album_id, set()).add(genre_id)
+
+    new_makealbum = []
+    new_albumgenre = []
+
+    for album_id, song_ids in album_songs.items():
+        needed_artists = set()
+        needed_genres = set()
+        for song_id in song_ids:
+            needed_artists.update(song_artists.get(song_id, set()))
+            needed_genres.update(song_genres.get(song_id, set()))
+
+        current_artists = album_artists.get(album_id, set())
+        current_genres = album_genres.get(album_id, set())
+
+        missing_artists = needed_artists - current_artists
+        missing_genres = needed_genres - current_genres
+
+        for artist_id in missing_artists:
+            new_makealbum.append((artist_id, album_id))
+        for genre_id in missing_genres:
+            new_albumgenre.append((album_id, genre_id))
+
+    if new_makealbum:
+        cur.executemany(
+            "INSERT INTO makealbum(artist_id, album_id) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+            new_makealbum,
+        )
+
+    if new_albumgenre:
+        cur.executemany(
+            "INSERT INTO albumhasgenre(album_id, genre_id) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+            new_albumgenre,
+        )
+        
     con.commit()
 
 def populate_is_part_of_collection(con, n=5000):
@@ -391,21 +517,22 @@ def main():
         start_ssh()
         con = get_con()
         
-        populate_users(con)
-        populate_artists(con)
-        populate_genres(con)
-        populate_songs(con)
-        populate_albums(con)
-        populate_collections(con)
-        populate_follow_users(con)
+        #populate_users(con)
+        #populate_artists(con)
+        #populate_genres(con)
+        #populate_songs(con)
+        #populate_albums(con)
+        #populate_collections(con)
+        #populate_follow_users(con)
         populate_make_song(con)
         populate_make_album(con)
         populate_is_part_of_album(con)
         populate_song_has_genre(con)
         populate_album_has_genre(con)
-        populate_is_part_of_collection(con)
-        populate_listen_to_song(con)
-        populate_song_rating(con)
+        sync_music(con)
+        #populate_is_part_of_collection(con)
+        #populate_listen_to_song(con)
+        #populate_song_rating(con)
         
         con.close()
         
